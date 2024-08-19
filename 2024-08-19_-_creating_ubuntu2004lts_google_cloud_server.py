3857269d2780b742c248f788eb5aee9af7829379
Edson Copque <ecop@disroot.org>
2024-08-19 20:13:36 -0300

    Update 2024-08-19_-_creating_ubuntu2004lts_google_cloud_server.py

--- 2024-08-19_-_creating_ubuntu2004lts_google_cloud_server.py
+++ 2024-08-19_-_creating_ubuntu2004lts_google_cloud_server.py
@@ -8,4 +8,12 @@
 # Agora você estará em Rede VPC, clique em Endereços Ip.
 # Na aula, no final do canto onde há o endereço ip externo há o botão Reservar (mas não achei). Então, leia abaixo.
 # Havia três pontinhos e a opção de "Promover a endereço ip estático". Isso mesmo, agora precisamos informar NOME (que será djangodeploy). Deu tudo certo. ;-)
-# 
+# No exemplo da aula, já constava as portas tcp: 80  e 443 liberadas, no meu não. No futuro provavelmente vou ter que CRIAR REGRAS DE FIREWALL.
+# Agora vamos em COMPUTE ENGINE > INSTÂNCIAS DE VM e em CONFIGURAÇÕES > METADADOS vamos em CHAVES SSH e clicar em EDITAR. (voltaremos em breve).
+# Vamos no terminal/bash e digitamos: ssh-keygen -C 'gnulinuxdebian'. Agora vai ser gerada uma chave (não mostre à ninguém). Vai aparecer uma confirmação de onde será salvo sua chave. Só dar ENTER. O endereço foi "/home/my_user/.ssh/id_rsa". Agora será solicitado uma senha. Decidi não usar senha alguma, apertei apenas ENTER. Pronto, suas chaves foram criadas.
+# Agora vamos abrir com editor de texto o arquivo id_rsa.pub, pois o usaremos no Google.
+# Ao abrir, selecione todo conteúdo e copie. 
+# Agora lá em CHAVES SSH (METADADOS), vamos em ADICIONAR ITEM e colamos a chave SSH e depois clicamos em SALVAR.
+# Agora vamos testar. No terminal digitaremos: ssh meu_usuario@ip_meu_servidor_externo (ssh gnulinuxdebian@34.28.163.xxx).
+# Seu terminal vai questionar se quer continuar a conexão. Digite yes.
+# Após isto, vai perceber que você está dentro do servidor. Em seu terminal vocÊ aparecerá assim: gnulinuxdebian@djangodeploy:~$.
