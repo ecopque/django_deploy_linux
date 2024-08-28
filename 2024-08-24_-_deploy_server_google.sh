2982f8729b5c9816ad8dbefe711f10f43a5e0310
Edson Copque <ecop@disroot.org>
2024-08-28 10:53:27 -0300

    Create 2024-08-24_-_deploy_server_google.sh

---
+++ 2024-08-24_-_deploy_server_google.sh
@@ -0,0 +1,13 @@
+# cd ~/scheduleapp
+# python3.11 -m venv venv #2:
+# source venv/bin/activate
+# pip install --upgrade pip #3:
+# pip install django
+# pip install pillow #4: 
+# pip install gunicorn #5:
+# pip install psycopg #6:
+# pip install faker #7:
+# python manage.py runserver #1: #8:
+# python manage.py migrate #9:
+# python manage.py collectstatic #10: 
+# python manage.py createsuperuser #11: #12:
